449bacf59607c42baa49d7086a3315d8392ca3b9
snailbaron <murbidodrus@gmail.com>
2017-07-24 02:44:56 +0300

    Start work on test map

---
+++ assets/maps/village/objects.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objects" tilewidth="500" tileheight="500" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="128" height="128" source="../../bitmaps/tree.png"/>
+ </tile>
+ <tile id="3">
+  <image width="500" height="500" source="../../bitmaps/house.png"/>
+ </tile>
+</tileset>
---
+++ assets/maps/village/terrain.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="terrain" tilewidth="64" tileheight="32" tilecount="16" columns="4">
+ <image source="../../tiles/grass.png" trans="ffffff" width="256" height="128"/>
+ <terraintypes>
+  <terrain name="grass" tile="14"/>
+  <terrain name="road" tile="5"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,1"/>
+ <tile id="1" terrain="0,0,1,1"/>
+ <tile id="2" terrain="0,0,1,0"/>
+ <tile id="3" terrain="1,1,1,0"/>
+ <tile id="4" terrain="0,1,0,1"/>
+ <tile id="5" terrain="1,1,1,1"/>
+ <tile id="6" terrain="1,0,1,0"/>
+ <tile id="7" terrain="1,1,0,1"/>
+ <tile id="8" terrain="0,1,0,0"/>
+ <tile id="9" terrain="1,1,0,0"/>
+ <tile id="10" terrain="1,0,0,0"/>
+ <tile id="11" terrain="1,0,1,1"/>
+ <tile id="12" terrain="0,1,1,0"/>
+ <tile id="13" terrain="1,0,0,1"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,1,1,1"/>
+</tileset>
